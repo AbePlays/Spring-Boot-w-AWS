65b181d5680ede7790ca7fd832bae1e3b75ef319
Abe <abhi.rawat456@gmail.com>
2021-01-25 19:19:13 +0530

    Implementing react-dropzone package

--- src/main/frontend/src/components/UserProfile/UserProfile.js
+++ src/main/frontend/src/components/UserProfile/UserProfile.js
@@ -1,3 +1,25 @@
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+const Dropzone = () => {
+  const onDrop = useCallback((acceptedFiles) => {
+    const file = acceptedFiles[0];
+    console.log(file);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
+};
+
 const UserProfile = ({ userProfiles }) => {
   return (
     <div>
@@ -5,6 +27,7 @@ const UserProfile = ({ userProfiles }) => {
         return (
           <div key={userProfile.userProfileId}>
             <h1>{userProfile.userName}</h1>
+            <Dropzone />
           </div>
         );
       })}
