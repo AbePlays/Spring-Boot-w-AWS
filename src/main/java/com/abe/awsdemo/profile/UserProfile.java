879c94e259850a906cdc8550b00b9f31671ee9f6
Abe <abhi.rawat456@gmail.com>
2021-01-25 11:34:06 +0530

    Created User Model

---
+++ src/main/java/com/abe/awsdemo/profile/UserProfile.java
@@ -0,0 +1,56 @@
+package com.abe.awsdemo.profile;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class UserProfile {
+
+    private UUID userProfileId;
+    private String userName;
+    private String userProfileImageLink;
+
+    public UserProfile(UUID userProfileId, String userName, String userProfileImageLink) {
+        this.userProfileId = userProfileId;
+        this.userName = userName;
+        this.userProfileImageLink = userProfileImageLink;
+    }
+
+    public UUID getUserProfileId() {
+        return userProfileId;
+    }
+
+    public void setUserProfileId(UUID userProfileId) {
+        this.userProfileId = userProfileId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserProfileImageLink() {
+        return userProfileImageLink;
+    }
+
+    public void setUserProfileImageLink(String userProfileImageLink) {
+        this.userProfileImageLink = userProfileImageLink;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserProfile that = (UserProfile) o;
+        return Objects.equals(userProfileId, that.userProfileId) &&
+                Objects.equals(userName, that.userName) &&
+                Objects.equals(userProfileImageLink, that.userProfileImageLink);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userProfileId, userName, userProfileImageLink);
+    }
+}
