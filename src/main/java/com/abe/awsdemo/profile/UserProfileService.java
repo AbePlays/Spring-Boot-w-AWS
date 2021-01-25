0c2cd4265a03f418f3b3be6ff79f1232bd3351b2
Abe <abhi.rawat456@gmail.com>
2021-01-25 11:50:55 +0530

    Added Rest Controller

---
+++ src/main/java/com/abe/awsdemo/profile/UserProfileService.java
@@ -0,0 +1,21 @@
+package com.abe.awsdemo.profile;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserProfileService {
+
+    private final UserProfileDataAccessService userProfileDataAccessService;
+
+    @Autowired
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+        this.userProfileDataAccessService = userProfileDataAccessService;
+    }
+
+    List<UserProfile> getUserProfiles() {
+        return userProfileDataAccessService.getUserProfiles();
+    }
+}
