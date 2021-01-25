e483f7f618f0a8442b1594b16e3d9a9d867a0987
Abe <abhi.rawat456@gmail.com>
2021-01-25 11:27:05 +0530

    Added save function to filestore

---
+++ src/main/java/com/abe/awsdemo/filestore/FileStore.java
@@ -0,0 +1,41 @@
+package com.abe.awsdemo.filestore;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+public class FileStore {
+
+    private final AmazonS3 s3;
+
+    @Autowired
+    public FileStore(AmazonS3 s3) {
+        this.s3 = s3;
+    }
+
+    public void save(String path,
+                     String fileName,
+                     Optional<Map<String, String>> optionalMetaData,
+                     InputStream inputStream) {
+        ObjectMetadata metaData = new ObjectMetadata();
+
+        optionalMetaData.ifPresent(map -> {
+            if(!map.isEmpty()) {
+                map.forEach(metaData::addUserMetadata);
+            }
+        });
+
+        try {
+            s3.putObject(path, fileName, inputStream, metaData);
+        } catch (AmazonServiceException e) {
+            throw new IllegalStateException("Failed to store data to s3", e);
+        }
+    }
+}
